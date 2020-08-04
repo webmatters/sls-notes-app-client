c1bd6b4ae67391aab167c2f9bd00a0b4b03ced84
Andrew Smith <andrew.smith@webmatters.io>
2020-08-04 15:58:14 -0400

    Finishes up react app.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "sls-stack-notes-uploads"
@@ -12,5 +13,6 @@ export default {
     USER_POOL_ID: "us-east-1_rMIy0iriI",
     APP_CLIENT_ID: "2gb3s3op4gmaiama5i86ivf7ft",
     IDENTITY_POOL_ID: "us-east-1:cd5de24d-fe4e-4f3d-96e4-41d29c82bba8"
-  }
+  },
+  STRIPE_KEY: "pk_test_A71jVdnMC5Gm4vkcf9VpErgH00EhAfcuY2"
 };
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Auth } from "aws-amplify";
-import { useHistory } from "react-router-dom";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
 import LoaderButton from "../components/LoaderButton";
 import { useAppContext } from "../libs/contextLib";
 import { useFormFields } from "../libs/hooksLib";
@@ -9,7 +9,6 @@ import { onError } from "../libs/errorLib";
 import "./Login.css";
 
 export default function Login() {
-  const history = useHistory();
   const { userHasAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(false);
   const [fields, handleFieldChange] = useFormFields({
@@ -29,7 +28,6 @@ export default function Login() {
     try {
       await Auth.signIn(fields.email, fields.password);
       userHasAuthenticated(true);
-      history.push("/");
     } catch (e) {
       onError(e);
       setIsLoading(false);
